be1488a6bbe0e2d00c0f42cc1dffab72ace02090
kaustavhalder <khalder.08@gmail.com>
2022-06-01 13:39:31 -0500

    code formatted

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
+// Custom endpoints
 
-// Custom endpoints 
+const healthRoute = require("./routes/health");
+const authRouter = require("./routes/authRouter");
 
-const healthRoute = require('./routes/health')
-const authRouter = require('./routes/authRouter')
-
-
-app.get('/',(req,res) => {
-    console.log("Root endpoint has been hit")
-    res.send("Its Alive")
-})
-
+app.get("/", (req, res) => {
+  console.log("Root endpoint has been hit");
+  res.send("Its Alive");
+});
 
 // Custom Middleware Functions
-app.use('/health',healthRoute)
-app.use('/auth',authRouter)
+app.use("/health", healthRoute);
+app.use("/auth", authRouter);
 
 app.listen(port, () => {
-    console.log(`Server is up and running on port ${port}`)
-})
+  console.log(`Server is up and running on port ${port}`);
+});
