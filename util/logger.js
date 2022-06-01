fc31064762de824872f6a72dd67eac857ef373ce
kaustavhalder <khalder.08@gmail.com>
2022-06-01 13:51:02 -0500

    added logger

--- util/logger.js
+++ util/logger.js
@@ -1,39 +1,36 @@
-const winston = require('winston')
-const dotenv = require('dotenv');
-const { transports } = require('winston');
+const winston = require("winston");
+const dotenv = require("dotenv");
+const { transports } = require("winston");
 
 dotenv.config({
-    path: '../config/local/.env'
-})
+  path: "../config/local/.env",
+});
 
 const logOptions = {
-    file : {
-        level : "info",
-        filename : '../logs/app.log',
-        handleExceptions : true,
-        json : true,
-        maxsize : 5242880,
-        maxFiles : 5,
-        colorize: false
-
-    },
-    console : {
-        level : process.env.LOG_LEVEL,
-        handleExceptions : true,
-        json : true,
-        colorize : false
-    }
+  file: {
+    level: "info",
+    filename: "../logs/app.log",
+    handleExceptions: true,
+    json: true,
+    maxsize: 5242880,
+    maxFiles: 5,
+    colorize: false,
+  },
+  console: {
+    level: process.env.LOG_LEVEL,
+    handleExceptions: true,
+    json: true,
+    colorize: false,
+  },
 };
 
-
-
 const logger = winston.createLogger({
-    level : 'info',
-    format : winston.format.json(),
-    transports : [
-        new winston.transports.File(logOptions.file),
-        new winston.transports.Console(logOptions.console)
-    ]
-})
+  level: "info",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File(logOptions.file),
+    new winston.transports.Console(logOptions.console),
+  ],
+});
 
-module.exports = logger
+module.exports = logger;
