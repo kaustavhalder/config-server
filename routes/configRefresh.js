ed1fa9f59d4733d214e25388454da29f7ad0c588
kaustavhalder <khalder.08@gmail.com>
2022-06-06 19:50:34 -0500

    updated routes

---
+++ routes/configRefresh.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+router.route('/').post( (req, res) => {
+    console.log("Config Refresh Endpoint has been hit")
+})
+
+
+moduele.exports = router
