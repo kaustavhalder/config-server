7edfb555e8759d384bd775131173f9e61a82a3f0
kaustavhalder <khalder.08@gmail.com>
2022-08-02 22:19:33 -0500

    added payload URI

---
+++ models/payloadModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const payloadSchema = new mongoose.Schema({
+  fileName: {
+    type: String,
+    required: [true, "A file name must be provided for config loading"],
+  },
+  repoUri: {
+    type: String,
+    required: [true, "GitHub URI must be provided for API to work "],
+  },
+});
